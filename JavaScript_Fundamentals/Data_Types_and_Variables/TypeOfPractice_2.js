33d3406e410d43f0ac22fb5d83b6ae187d2260dc
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2014-08-14 16:16:29 +0300

    Create TypeOfPractice_2.js

---
+++ JavaScript_Fundamentals/Data_Types_and_Variables/TypeOfPractice_2.js
@@ -0,0 +1,9 @@
+/* The statement of the problem:
+   Create null, undefined variables and try typeof on them.
+*/
+
+var undefinedVariable;
+var nullVariable = null;
+
+console.log(typeof (undefinedVariable));
+console.log(typeof (nullVariable));
