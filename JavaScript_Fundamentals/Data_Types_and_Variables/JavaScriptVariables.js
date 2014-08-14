682f7d320a2ff04dd764fbd160ca37787ea5f41c
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2014-08-14 16:11:55 +0300

    Create JavaScriptVariables.js

---
+++ JavaScript_Fundamentals/Data_Types_and_Variables/JavaScriptVariables.js
@@ -0,0 +1,9 @@
+/* The statement of the problem:
+   Assign all the possible JavaScript literals to different variables.
+*/
+
+var intNumber = 17;
+var floatNumber = 2.718281;
+var isJavaScriptAwesome = true;
+var stringVariable = "Hello, JavaScript!";
+var x;
