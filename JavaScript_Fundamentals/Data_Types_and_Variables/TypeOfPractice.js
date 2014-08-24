0ac30e227439f900d0bd3578553f21cc9e5b34a7
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2014-08-24 13:51:47 +0300

    Create TypeOfPractice.js

---
+++ JavaScript_Fundamentals/Data_Types_and_Variables/TypeOfPractice.js
@@ -0,0 +1,15 @@
+/* The statement of the problem:
+   Try typeof on all variables you created.
+*/
+
+var intNumber = 17;
+var floatNumber = 2.718281;
+var isJavaScriptAwesome = true;
+var stringVariable = "Hello, JavaScript!";
+var x;
+
+console.log(typeof (intNumber));
+console.log(typeof (floatNumber));
+console.log(typeof (isJavaScriptAwesome));
+console.log(typeof (stringVariable));
+console.log(typeof (x));
