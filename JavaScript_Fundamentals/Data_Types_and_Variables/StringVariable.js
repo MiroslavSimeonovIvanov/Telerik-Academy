ce473dacac96f37c60c420e1dff681a23ddeaac7
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2014-08-24 13:44:43 +0300

    Delete StringVariable.js

--- JavaScript_Fundamentals/Data_Types_and_Variables/StringVariable.js
+++
@@ -1,6 +0,0 @@
-/* The statement of the problem:
-   Create a string variable with quoted text in it.
-   For example: 'How you doin'?', Joey said.
-*/
-
-var quotedText = "'How you doin'?', Joey said.'";
