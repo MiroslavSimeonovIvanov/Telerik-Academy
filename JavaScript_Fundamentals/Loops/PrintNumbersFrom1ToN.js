7b74578792a6d0993eb9614e9b2ad4a3b72c6747
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2014-10-24 16:33:18 +0300

    Create PrintNumbersFrom1ToN.js

---
+++ JavaScript_Fundamentals/Loops/PrintNumbersFrom1ToN.js
@@ -0,0 +1,11 @@
+/* The statement of the problem:
+   Write a script that prints all the numbers from 1 to N.
+*/
+
+// Reading the input data.
+var numberN = Number(window.prompt("Hello user! Please enter a number", ""));
+
+// With for loop we print the result.
+for (var numbers = 1; numbers <= numberN; numbers++) {
+    console.log(numbers);
+}
