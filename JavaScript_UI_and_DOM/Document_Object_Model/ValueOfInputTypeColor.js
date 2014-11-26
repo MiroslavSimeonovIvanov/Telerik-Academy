7d7014fab054a52ff3d70ea048f076e164e46f27
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2014-11-26 21:34:06 +0200

    Delete ValueOfInputTypeColor.js

--- JavaScript_UI_and_DOM/Document_Object_Model/ValueOfInputTypeColor.js
+++
@@ -1,9 +0,0 @@
-/* The statement of the problem:
-   Create a function that gets the value of <input
-   type="color" /> and sets the background of the body
-   to this value.
-*/
-
-function getValueOfInputTypeColor() {    
-    document.body.style.backgroundColor = document.getElementById("color").value;
-}
