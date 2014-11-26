d14cbcf40ee9fbaa8a6e6ed2a0e9026ee4c5af94
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2014-11-26 21:33:59 +0200

    Delete ValueOfInputTypeText.js

--- JavaScript_UI_and_DOM/Document_Object_Model/ValueOfInputTypeText.js
+++
@@ -1,9 +0,0 @@
-/* The statement of the problem:
-   Create a function that gets the value of <input
-   type="text" /> and prints its value to the console. 
-*/
-
-function getValueOfInputTypeText() {
-    var inputTypeText = document.getElementById("text").value;
-    document.getElementById("result").innerHTML = inputTypeText;
-}
