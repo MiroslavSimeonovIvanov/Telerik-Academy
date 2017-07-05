63ec489d6314c7b3190b4f2658a281711764c5e7
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2017-07-05 21:53:59 +0300

    Create 02.RefactorCSharpSourceCode.cs

---
+++ High-Quality_Programming_Code/Using_Control_Structures_Conditional_Statements_and_Loops/02.RefactorCSharpSourceCode.cs
@@ -0,0 +1,24 @@
+using System;
+
+class IfStatements
+{
+    // The first if statement.
+    Potato potato;
+
+    if (potato != null)
+	  {
+	      if (potato.IsPeeled && potato.IsNotRotten)
+	      {
+            Cook(potato);
+	      }	 
+	  }
+
+    // The second if statement.
+    bool xInRange = MIN_X <= x && x <= MAX_X;
+    bool yInRange = MIN_Y <= y && y <= MAX_Y;
+
+    if (xInRange && yInRange && shouldVisitCell)
+    {
+        VisitCell();
+    }
+}
