31fac5a76a8208b46e9bdec64e0d8bc03e7904e4
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2018-03-09 21:54:00 +0200

    Create 19.NewRecordsInUsersAndGroupsTables.sql

---
+++ Databases_and_SQL_Course/Advanced_SQL/19.NewRecordsInUsersAndGroupsTables.sql
@@ -0,0 +1,6 @@
+INSERT INTO [TelerikAcademy].[dbo].[Users]
+VALUES ('Superman', 'AC#11938', 'Kal-El', '2017-11-15 21:00', 5),
+	('Harley Quinn', 'BATMAN: THE ANIMATED SERIES (1992)', 'Harleen Quinzel', '2016-08-01 22:32:57', 4);
+
+INSERT INTO [TelerikAcademy].[dbo].[Groups]
+VALUES ('Teen Titans');
